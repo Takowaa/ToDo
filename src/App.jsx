6db2b60e623adea636a0f3164574ce9f62df4aa5
taalai123 <tako250494@gmail.com>
2023-12-01 12:09:03 +0600

    add buttons and upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import {nanoid} from "nanoid";
 function App() {
   const [value, setValue] = useState('')
   const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || [])
+  const [editableItem, seteditableItem] = useState(null)
 
+  const handleChange = (e) =>{
+    setValue(e.target.value)
+  }
   const createItem =() =>{
     const newTodo = {
       title: value,
@@ -13,14 +17,28 @@ function App() {
     localStorage.setItem('todos', JSON.stringify([...todos, newTodo]))
     setValue('')
   }
+
   const deleteItem = (id) => {
     const filteredLIst = todos.filter((el) => el.id !== id)
     localStorage.setItem('todos', JSON.stringify(filteredLIst))
     setTodos(filteredLIst)
   }
-  const handleChange = (e) =>{
-    setValue(e.target.value)
+  const editItem = (todo) =>{
+    seteditableItem(todo)
   }
+  const editCancallation = () =>{
+    seteditableItem(null)
+  }
+  const handleChangeEdit = (e) =>{
+    seteditableItem({...editableItem, title: e.target.value})
+  }
+  const saveItem = () =>{
+    const updateList = todos.map(elem => elem.id === editableItem.id ? editableItem : elem)
+    localStorage.setItem('todos', JSON.stringify(updateList))
+    setTodos(updateList)
+    seteditableItem(null)
+  }
+
   return (
     <>
         <h1 className='text-center mt-5 fw-bold'>ToDo</h1>
@@ -33,8 +51,24 @@ function App() {
        <ul className="list-group mt-3">
          {
            todos.map((todo)=>(
-             <li className="list-group-item d-flex justify-content-between" key={todo.id}>{todo.title}
-              <button className="btn btn-danger btn-sm" onClick={()=> deleteItem(todo.id)}> Delete</button>
+             <li className="list-group-item d-flex justify-content-between" key={todo.id}>
+               {
+                  editableItem?.id === todo.id ?
+                   <input defaultValue={todo.title} className="form-control-sm" type="text" onChange={handleChangeEdit}/>
+                   : todo.title
+               }
+               {
+              editableItem?.id === todo.id ?
+                   <div>
+                     <button className="btn btn-warning btn-sm" onClick={saveItem}> Save</button>
+                     <button className="btn btn-danger btn-sm ms-2" onClick={editCancallation}> Cancel</button>
+                   </div>
+                   :
+                   <div>
+                     <button className="btn btn-warning btn-sm" onClick={()=> editItem(todo)}> Edit</button>
+                     <button className="btn btn-danger btn-sm ms-2" onClick={()=> deleteItem(todo.id)}> Delete</button>
+                   </div>
+               }
              </li>
            ))
          }
